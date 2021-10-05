d48795e92bdf7e516dcc1b2974e50b22d82ed7cc
George Jpegov <48419012+g3orgeddev@users.noreply.github.com>
2021-10-06 00:15:16 +0300

    Add files via upload

---
+++ js/resizeScript.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function() {
+    var mediaMax = window.matchMedia('(max-width: 899px)');
+    var mediaMin = window.matchMedia('(min-width: 901px)');
+
+    if (mediaMax.matches) {
+        document.getElementById("artist").className = "form-control form-control-lg";
+        document.getElementById("title").className = "form-control form-control-lg";
+        document.getElementById("show_lyrics").className = "btn btn-success btn-lg";
+    } else if (mediaMin.matches) {
+        document.getElementById("artist").className = "form-control";
+        document.getElementById("title").className = "form-control";
+        document.getElementById("show_lyrics").className = "btn btn-success";
+    }
+});
+
+window.addEventListener('resize', function() {
+    var mediaMax = window.matchMedia('(max-width: 899px)');
+    var mediaMin = window.matchMedia('(min-width: 901px)');
+
+    if (mediaMax.matches) {
+        document.getElementById("artist").className = "form-control form-control-lg";
+        document.getElementById("title").className = "form-control form-control-lg";
+        document.getElementById("show_lyrics").className = "btn btn-success btn-lg";
+    } else if (mediaMin.matches) {
+        document.getElementById("artist").className = "form-control";
+        document.getElementById("title").className = "form-control";
+        document.getElementById("show_lyrics").className = "btn btn-success";
+    }
+});
