ad7acfa9c0df82d0bbb73ca4ffdb03a2205586c7
George Jpegov <48419012+g3orgeddev@users.noreply.github.com>
2021-10-06 00:27:43 +0300

    minor fixes

--- js/resizeScript.js
+++ js/resizeScript.js
@@ -12,6 +12,7 @@ window.addEventListener('load', function() {
         document.getElementById("show_lyrics").className = "btn btn-success";
     }
 });
+
 window.addEventListener('resize', function() {
     var mediaMax = window.matchMedia('(max-width: 899px)');
     var mediaMin = window.matchMedia('(min-width: 901px)');
