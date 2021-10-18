142d28ab5d52a6b430544bfa849299648afc5371
George Jpegov <48419012+g3orgeddev@users.noreply.github.com>
2021-10-19 00:51:43 +0300

    some random fixes and code improvements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
-var artist, song;
-
 function getArtistAndSong() {
-    artist = document.getElementById("artist").value.replace(/ /g, "%20");
-    title = document.getElementById("title").value.replace(/ /g, "%20");;
+    var artist = document.getElementById("artist").value.replace(/ /g, "%20");
+    var title = document.getElementById("title").value.replace(/ /g, "%20");;
 
-    strFetch = "https://api.lyrics.ovh/v1/"+artist+"/"+title;
-    //console.log(strFetch);
+    strFetch = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
+    console.log(strFetch);
 
-    $.get(strFetch, function(data){
-        if (data.error === "No lyrics found") {
-            document.getElementById("lyricfy_lyrics").innerHTML = "Oops, something went wrong... No lyrics found."
-        } else {
-            document.getElementById("lyricfy_lyrics").innerHTML = data.lyrics.replace(new RegExp("\n", "g"), "<br class='unselectable'>")
-            document.getElementById("lyricfy_lyrics").className = "lyricfy_lyrics_shown";
-        }
+    $.get(strFetch, function(data) {
+        var lyrics = data.lyrics.replace(/Paroles de la chanson\s.*\spar\s.*\r\n/gm, "");
+        
+        document.getElementById("lyricfy_lyrics").innerHTML = lyrics.replace(new RegExp("\n", "g"), "<br class='unselectable'>")
+        document.getElementById("lyricfy_lyrics").className = "lyricfy_lyrics_shown";
     })
 }
