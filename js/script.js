641b45660fa210416e4770ed959f1bc42e580b1c
g3orgedd <48419012+g3orgedd@users.noreply.github.com>
2021-10-19 18:35:40 +0300

    Major fixes and code improvement
    - Function "getArtistAndSong" has been renamed to "showLyrics"
    - Added new function "changeClassName"
    - Major changes in the logic of the "getLyrics" function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-function getArtistAndSong() {
+function showLyrics() {
     var artist = document.getElementById("artist").value.replace(/ /g, "%20");
     var title = document.getElementById("title").value.replace(/ /g, "%20");;
 
     strFetch = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
     console.log(strFetch);
 
-    $.get(strFetch, function(data) {
-        var lyrics = data.lyrics.replace(/Paroles de la chanson\s.*\spar\s.*\r\n/gm, "");
-        
-        document.getElementById("lyricfy_lyrics").innerHTML = lyrics.replace(new RegExp("\n", "g"), "<br class='unselectable'>")
-        document.getElementById("lyricfy_lyrics").className = "lyricfy_lyrics_shown";
-    })
+    getLyrics(strFetch);
+}
+
+function changeClassName() {
+    document.getElementById("lyricfy_lyrics").className = "lyricfy_lyrics_shown";
+}
+
+function getLyrics(strFetch) {
+    var request = new XMLHttpRequest();
+    
+    request.open('GET', strFetch, true);
+    request.responseType = 'json';
+
+    request.onload = function () {
+        if (this.readyState === 4) {
+            if (this.status != 200) {
+                document.getElementById("lyricfy_lyrics").innerHTML = "Oops, something went wrong... No lyrics found.";  
+                changeClassName();
+            } else {
+                var lyrics = request.response['lyrics'].replace(/Paroles de la chanson\s.*\spar\s.*\r\n/gm, "");
+                
+                document.getElementById("lyricfy_lyrics").innerHTML = lyrics.replace(new RegExp("\n", "g"), "<br class='unselectable'>");              
+                changeClassName();
+            } 
+        }
+    };
+    request.send();
 }
